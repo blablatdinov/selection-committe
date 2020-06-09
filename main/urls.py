1fd6f044c71bd68402f8391a9fcdcbdb1ade8d8e
Almaz <a.ilaletdinov@yandex.ru>
2020-06-09 11:17:06 +0300

    Add login system

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
+from django.contrib.auth.views import LoginView
 from main.views import main
 
 
 urlpatterns = [
-	path('', main)
+	path('', main),
+	path('login', LoginView.as_view(template_name='main/auth.html', redirect_field_name='profile'), name='login'),
 ]
